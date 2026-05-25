2503361dd9506698b970366d5a62b08e9bae0482
Lucas Gabriel <lucasgabriel19672@gmail.com>
2026-05-25 20:51:48 -0300

    Add files via upload

---
+++ atividadesenha/src/main/java/com/example/atividadesenha/model/Usuario.java
@@ -0,0 +1,95 @@
+package com.example.atividadesenha.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String login;
+    private String email;
+    private String senha;
+    private TipoUsuario usuario;
+    private int tentativasFalhas = 0;
+    private boolean bloqueado = false;
+
+    public Usuario() {
+    }
+
+    public Usuario(Long id, String nome, String login, String email, String senha, TipoUsuario usuario, int tentativasFalhas, boolean bloqueado) {
+        this.id = id;
+        this.nome = nome;
+        this.login = login;
+        this.email = email;
+        this.senha = senha;
+        this.usuario = usuario;
+        this.tentativasFalhas = tentativasFalhas;
+        this.bloqueado = bloqueado;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public TipoUsuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(TipoUsuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public int getTentativasFalhas() {
+        return tentativasFalhas;
+    }
+
+    public void setTentativasFalhas(int tentativasFalhas) {
+        this.tentativasFalhas = tentativasFalhas;
+    }
+
+    public boolean isBloqueado() {
+        return bloqueado;
+    }
+
+    public void setBloqueado(boolean bloqueado) {
+        this.bloqueado = bloqueado;
+    }
+}
