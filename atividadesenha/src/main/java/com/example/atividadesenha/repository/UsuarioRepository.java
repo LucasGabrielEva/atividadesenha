7c0ad35c66bca9389b6b6b410dc7f3df9e42d6fe
Lucas Gabriel <lucasgabriel19672@gmail.com>
2026-05-25 20:39:35 -0300

    Delete atividadesenha directory

--- atividadesenha/src/main/java/com/example/atividadesenha/repository/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.atividadesenha.repository;
-
-import com.example.atividadesenha.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
-}
